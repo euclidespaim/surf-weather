cfea186c6d90613b83f6a5f091e376d4b62accb0
euclidespaim <euclidespaim@gmail.com>
2020-08-04 10:24:10 -0300

    Surf forecast fetch geoapi! Vulnerability checked!

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const tempElement = document.querySelector(".temperature-value p");
 const descElement = document.querySelector(".temperature-description p");
 const locationElement = document.querySelector(".location p");
 const notificationElement = document.querySelector(".notification");
+const waveElement = document.querySelector(".wave-height p");
+const tideElement = document.querySelector(".tide-direction p");
+const windElement = document.querySelector(".wind-direction p");
 
 // App data
 const weather = {};
@@ -25,6 +28,11 @@ if('geolocation' in navigator){
 }else{
     notificationElement.style.display = "block";
     notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
+}
+//SET HOUR OF FORECAST
+function setTime(){
+    let time = new Date();
+    return Math.floor(time.getHours() /3);
 }
 
 // SET USER'S POSITION
@@ -45,33 +53,49 @@ function showError(error){
 function getWeather(latitude, longitude){
 
     let api = `http://api.worldweatheronline.com/premium/v1/marine.ashx?key=${key}&format=json&q=${latitude},${longitude}`;
-    let geoapi = `http://www.mapquestapi.com/geocoding/v1/reverse?key=${geokey}&location=${latitude},${longitude}&outFormat=xml`;
+    let geoapi = `http://www.mapquestapi.com/geocoding/v1/reverse?key=${geokey}&location=${latitude},${longitude}&outFormat=json`;
+
+    let hour = setTime();
 
     //Fetch json from worldWeatherOnline
     fetch(api)
         .then(function(response){
-            let data = response.json();
-            return data;
+            let world = response.json();
+            return world;
         })
-        .then(function(data){
+        .then(function(world){
 
-            weather.temperature.value = data.data.weather[0].hourly[0].tempC;
-            weather.iconId = data.data.weather[0].astronomy[0].moon_phase;
+            weather.temperature.value = world.data.weather[0].hourly[hour].tempC;
+            weather.iconId = world.data.weather[0].astronomy[0].moon_phase;
             weather.iconId = weather.iconId.replace(/\s/g, '');
-            weather.description = data.data.weather[0].hourly[0].weatherDesc[0].value;
 
+            weather.description = world.data.weather[0].hourly[hour].weatherDesc[0].value;
+            weather.wave = world.data.weather[0].hourly[hour].swellHeight_m;
+            weather.tide = world.data.weather[0].hourly[hour].swellDir16Point;
+            weather.wind = world.data.weather[0].hourly[hour].winddir16Point;
+        });
+
+    fetch(geoapi)
+        .then(function (response){
+            let quest = response.json();
+            return quest;
+        })
+        .then(function (quest){
+
+            weather.city = quest.results[0].locations[0].adminArea5;
+            weather.country = quest.results[0].locations[0].adminArea1;
         })
         .then(function(){
             displayWeather();
         });
 
-    //Request xml from mapQuestApi
-    const request = new XMLHttpRequest();
-    request.open("GET", geoapi, false);
-    request.send();
-    const xml = request.responseXML;
-    weather.city = xml.getElementsByTagName("adminArea5")[0].textContent;
-    weather.country = xml.getElementsByTagName("adminArea1")[0].textContent;
+    // //Request xml from mapQuestApi
+    // const request = new XMLHttpRequest();
+    // request.open("GET", geoapi, false);
+    // request.send();
+    // const xml = request.responseXML;
+    // weather.city = xml.getElementsByTagName("adminArea5")[0].textContent;
+    // weather.country = xml.getElementsByTagName("adminArea1")[0].textContent;
 
 }
 
@@ -81,6 +105,9 @@ function displayWeather(){
     tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
     descElement.innerHTML = weather.description;
     locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+    waveElement.innerHTML = `${weather.wave} <span>m</span>`;
+    tideElement.innerHTML = `${weather.tide}`;
+    windElement.innerHTML = `${weather.wind}`;
 }
 
 // C to F conversion
@@ -88,7 +115,7 @@ function celsiusToFahrenheit(temperature){
     return (temperature * 9/5) + 32;
 }
 
-// WHEN THE USER CLICKS ON THE TEMPERATURE ELEMENET
+// WHEN THE USER CLICKS ON THE TEMPERATURE ELEMENT
 tempElement.addEventListener("click", function(){
     if(weather.temperature.value === undefined) return;
 
